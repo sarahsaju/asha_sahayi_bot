7f6d8e59f5ec6f7ee5bed6a391231246b4e070c5
Sarah Saju Muhammed <sarahsaju@example.com>
2026-01-12 18:44:04 +0530

    basic text message handling

--- bot.py
+++ bot.py
@@ -1,18 +1,31 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
 BOT_TOKEN = "8100918560:AAFO3g_J19HMRfDIWOlULKJN6_PVwCUYkV4"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "നമസ്കാരം 🙏\n"
-        "ഞാൻ ആശാ സഹായി.\n"
-        "നിങ്ങളെ സഹായിക്കാൻ ഞാൻ ഇവിടെ ഉണ്ട്. \n"
+        "ഞാൻ ASHA സഹായി ആണ്.\n"
+        "ആരോഗ്യ വിവരങ്ങൾക്ക് ഞാൻ നിങ്ങളെ സഹായിക്കും. \n"
         "എന്ത് സഹായം വേണമെന്ന് പറയൂ."
     )
 
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_text = update.message.text
+
+    reply = (
+        "നിങ്ങളുടെ സന്ദേശം ലഭിച്ചു.\n"
+        "ഇപ്പോൾ ഞാൻ പരിശീലന ഘട്ടത്തിലാണ്.\n"
+        "ഉടൻ തന്നെ ആരോഗ്യ വിവരങ്ങൾ നൽകാൻ കഴിയും."
+    )
+
+    await update.message.reply_text(reply)
+
 app = ApplicationBuilder().token(BOT_TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
 print("🤖 ASHA Sahayi bot is running...")
 app.run_polling()
